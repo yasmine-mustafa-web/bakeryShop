9d2bb926bf99b4216ed113b0f148aa5a0c4e406e
Esraa <essomostafa57@gmail.com>
2026-04-16 22:32:42 +0200

    update

--- app.js
+++ app.js
@@ -35,12 +35,11 @@ async function loadMenu() {
        <img class="imgCroissants" src="${item.photo}" alt="${item.name}">
        <div class="inf">
        <h3 class="nameItem"> ${item.name}</h3>
-       <p class="descr">${item.descr}</p>
-       <button class="price">🛒${item.price}</button>
+       <p class="price">${item.price}</p>
 
        </div>`;
 
-       const btnPrics=card.querySelector(".price");
+       const btnPrics=card.querySelector(".imgCroissants");
 btnPrics.addEventListener("click", () =>{
     showDetails(item);
 }
@@ -67,3 +66,95 @@ function showDetails(item){
 
 }
 loadMenu();
+
+  let qnty = 1;
+    const savedItem = localStorage.getItem("selectedItem");
+    if (savedItem) {
+      const item = JSON.parse(savedItem);
+      const pricecontainer = document.querySelector(".pricecontainer");
+
+      pricecontainer.innerHTML = `
+            <section class="seperate">
+                           <div>
+                            <img class="imgCroissants2" src="${item.photo}">
+</div>
+            <div class="priceDiv">
+                <div>
+                                  <h2 class="nameItem2">${item.name}</h2>
+                                  <p class="price">${item.price}</p>
+                                  <p>Tax included fees are calcualted at checkout.</p>
+                                  <p style="font-weight:bold;">Qunatity:</p>
+                                
+                                  <button class="qty">1</button>
+                                    <DIV class="btnHolders">
+                                  <button class="cart">ADD TO CART</button>
+                                                                    <button class="cta">BUY IT NOW</button>
+                                                                    </div>
+
+             <p  class="descr2">${item.descr}</p>
+        
+                </section>
+ 
+         
+     
+            `;
+            const qtyBtn=document.querySelector(".qty");
+                                    let cartCount=0;
+
+            qtyBtn.addEventListener("click", ()=>{
+qnty++;
+qtyBtn.textContent=qnty;
+
+            })
+            const cartBtn=document.querySelector(".cart");
+
+cartBtn.addEventListener("click",()=>{
+  let addCart=document.querySelector(".countBtn");
+  cartCount++;
+  const bill= calcTotal(qnty);
+  
+  if(!addCart){
+   addCart=document.createElement("button")
+  addCart.className="countBtn";
+  addCart.style.borderRadius="30px";
+  addCart.style.padding="10px";
+addCart.style.float="right";
+addCart.style.margin="20px";
+addCart.style.border="none";
+addCart.style.backgroundColor="red";
+addCart.style.color="white";
+  document.body.appendChild(addCart);
+  }
+  addCart.textContent= cartCount + "🛒";
+document.querySelector(".subtotal").textContent=`$${bill.ttl}`;
+document.querySelector(".taxs").textContent=`$${bill.tax}`;
+document.querySelector(".totalbill").textContent=`$${bill.total}`;
+
+})
+      pricecontainer.scrollIntoView({ behavior: "smooth" });
+
+    } else {
+      console.error("No item found in localStorage");
+    }
+            let item;
+
+function calcTotal(qnty){
+    const ttl=item.price*qnty;
+    const taxRate = 0.14;
+    const tax=ttl*item.taxRate;
+    const total=ttl + tax;
+    return {
+        ttl: ttl.toFixed(2),
+        tax: tax.toFixed(2),
+        total: total.toFixed(2)
+    };
+}
+
+const cta=document.querySelector(".cta");
+cta.addEventListener("click",()=>{
+const bill=calcTotal(qnty||1);
+localStorage.setItem("userBill", JSON.stringify(bill));
+
+window.location.href="checkout.html";
+});
+
